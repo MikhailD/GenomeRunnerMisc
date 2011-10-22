5e3b23c1f5aaad89ca37e6c5c01f7654ec2a3982
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2011-10-22 11:38:18 -0500

    Initial commit

---
+++ GenomeRunnerMisc/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExtractAllGFs = New System.Windows.Forms.Button()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnExtractAllGFs
+        '
+        Me.btnExtractAllGFs.Location = New System.Drawing.Point(12, 45)
+        Me.btnExtractAllGFs.Name = "btnExtractAllGFs"
+        Me.btnExtractAllGFs.Size = New System.Drawing.Size(145, 55)
+        Me.btnExtractAllGFs.TabIndex = 0
+        Me.btnExtractAllGFs.Text = "Extract All GFs"
+        Me.btnExtractAllGFs.UseVisualStyleBackColor = True
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(12, 12)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 1
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(22, 191)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(497, 262)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.btnExtractAllGFs)
+        Me.Name = "Form1"
+        Me.Text = "GenomeRunnerMisc"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnExtractAllGFs As System.Windows.Forms.Button
+    Friend WithEvents btnConnect As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ GenomeRunnerMisc/Form1.vb
@@ -0,0 +1,137 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.IO
+
+Public Class Form1
+    Dim ConnectionString As String
+    Dim cn As MySqlConnection, cmd As MySqlCommand, dr As MySqlDataReader, cmd1 As MySqlCommand, dr1 As MySqlDataReader
+
+    Public Class genomerunnertable
+        Public ID As Integer
+        Public FeatureTable As String
+        Public FeatureName As String
+        Public Tier As Integer
+    End Class
+    Private Function GetConnectionSettings(ByRef uName As String, ByRef uPassword As String, ByRef uServer As String, ByRef uDatabase As String) As String
+        'gets the connection string values from the registry if they exist
+        Dim connectionString As String
+        Try
+            uName = GetSetting("GenomeRunner", "Database", "uName")
+            uPassword = GetSetting("GenomeRunner", "Database", "uPassword")
+            uServer = GetSetting("GenomeRunner", "Database", "uServer")
+            uDatabase = GetSetting("GenomeRunner", "Database", "uDatabase")
+        Catch
+            SaveSetting("GenomeRunner", "Database", "uName", "genomerunner")
+            SaveSetting("GenomeRunner", "Database", "uPassword", "genomerunner")
+            SaveSetting("GenomeRunner", "Database", "uServer", "156.110.144.34")
+            SaveSetting("GenomeRunner", "Database", "uDatabase", "hg18test")
+            uName = GetSetting("GenomeRunner", "Database", "uName")
+            uPassword = GetSetting("GenomeRunner", "Database", "uPassword")
+            uServer = GetSetting("GenomeRunner", "Database", "uServer")
+            uDatabase = GetSetting("GenomeRunner", "Database", "uDatabase")
+        End Try
+        connectionString = "Server=" & uServer & ";Database=" & uDatabase & ";User ID=" & uName & ";Password=" & uPassword
+        Return connectionString
+    End Function
+
+    Private Sub OpenDatabase()
+        Dim uName As String = ""
+        Dim uPassword As String = ""
+        Dim uServer As String = ""
+        Dim uDatabase As String = ""
+
+        ConnectionString = GetConnectionSettings(uName, uPassword, uServer, uDatabase)
+        'checks again to see if the values are still empty, if they are the connection is left blank and a connection is not oppened. 
+        Dim ConnectionWorks As Boolean = False
+        While ConnectionWorks = False
+            Try
+                cn = New MySqlConnection(ConnectionString) : cn.Open()
+                cmd = New MySqlCommand("SELECT id FROM genomerunner limit 1", cn)
+                dr = cmd.ExecuteReader()
+                ConnectionWorks = True
+                dr.Close()
+            Catch
+                frmLogin.ShowDialog()
+                ConnectionString = GetConnectionSettings(uName, uPassword, uServer, uDatabase)
+            End Try
+        End While
+
+    End Sub
+
+
+    Private Sub btnExtractAllGFs_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExtractAllGFs.Click
+        Dim GFs As New List(Of genomerunnertable)
+        Dim columnnames, columnsreturned As String
+        OpenDatabase()
+        cmd = New MySqlCommand("SELECT id,featuretable,featurename,tier FROM genomerunner", cn)
+        dr = cmd.ExecuteReader
+        While dr.Read
+            Dim tempGF As New genomerunnertable
+            With tempGF
+                .ID = dr(0) : .FeatureTable = dr(1) : .FeatureName = dr(2) : .Tier = dr(3)
+            End With
+            GFs.Add(tempGF)
+        End While
+        dr.Close() : cmd.Dispose()
+
+        For Each GF In GFs
+            If GF.ID >= 59 Then
+                Try
+                    cmd = New MySqlCommand("SHOW COLUMNS FROM " & GF.FeatureTable & ";", cn)
+                    dr = cmd.ExecuteReader : columnnames = vbNullString
+                    While dr.Read
+                        columnnames &= dr(0)
+                    End While
+                    dr.Close() : cmd.Dispose()
+
+                    Dim nameIndex As Integer = columnnames.ToLower().IndexOf("name")
+                    Dim strandIndex As Integer = columnnames.ToLower().IndexOf("strand")
+                    Dim geneIndex As Integer = columnnames.ToLower().IndexOf("txstart")
+                    columnsreturned = ""
+                    If geneIndex <> -1 Then
+                        columnsreturned = "chrom,txstart,txend"
+                    Else
+                        columnsreturned = "chrom,chromstart,chromend"
+                    End If
+                    If nameIndex <> -1 Then columnsreturned &= ",name"
+                    If strandIndex <> -1 Then columnsreturned &= ",strand"
+
+                    cmd = New MySqlCommand("SELECT " & columnsreturned & " FROM " & GF.FeatureTable & ";", cn)
+                    dr = cmd.ExecuteReader
+                    Using writer As New StreamWriter("F:\WorkOMRF\Gene Ontology\GenomeRunner_tests\All_GF\" & GF.ID & "-" & GF.FeatureTable & "-" & GF.Tier & ".bed")
+                        While dr.Read
+                            writer.Write(dr(0) & vbTab & dr(1) & vbTab & dr(2))
+                            If nameIndex <> -1 Then
+                                writer.Write(vbTab & dr(3))
+                            Else
+                                writer.Write(vbTab & "")
+                            End If
+                            If strandIndex <> -1 Then writer.Write(vbTab & dr(4))
+                            writer.Write(vbCrLf)
+                        End While
+                        dr.Close() : cmd.Dispose()
+                    End Using
+                    Debug.Print("Done - " & GF.ID)
+                Catch
+                    Debug.Print(GF.ID)
+                End Try
+            End If
+        Next
+        MsgBox("Success!!!")
+    End Sub
+
+    Private Sub btnConnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConnect.Click
+        frmLogin.ShowDialog()
+    End Sub
+
+    Function TrimStr(ByRef StringToTrim As String) As String
+        'http://www.vbdotnetforums.com/vb-net-general-discussion/31506-how-remove-all-special-characters-string-visual-basic-net.html
+        Dim illegalChars As Char() = "!@#$%^&*(){}[]""+'<>?/\:.-".ToCharArray() '"!@#$%^&*(){}[]""_+<>?/".ToCharArray() 
+        Dim sb As New System.Text.StringBuilder
+        For Each ch As Char In StringToTrim
+            If Array.IndexOf(illegalChars, ch) = -1 Then
+                sb.Append(ch)
+            End If
+        Next
+        Return sb.ToString
+    End Function
+End Class
